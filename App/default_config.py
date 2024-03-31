a05b5dee24ade6146d4590647308af3249b3cd67
Keshan Moosai <keshanmoosai@gmail.com>
2024-03-31 15:35:03 -0400

    added host ref to DTTEC-Persistent

--- App/default_config.py
+++ App/default_config.py
@@ -1,2 +1,3 @@
 SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
 SECRET_KEY = "secret key"
+DB_HOST = "postgresql://admin:Y2D4GTt9TAd6ABBzwrCxl702POpTNpU4@dpg-co4q72n79t8c7395qt5g-a.oregon-postgres.render.com/dttec"
